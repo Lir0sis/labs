adc02ac344d69d69f7557840f04dcd33feb1d0c5
Lir0sis <valikshonder@gmail.com>
2019-10-02 22:23:48 +0300

    lab3-4

---
+++ Programming Basics/lab_3/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isResultable = false;
+
+            double firstArgX = 0;
+
+            int days = 0;
+            double discount = 0f;
+            double sum = 0f;
+
+            double secondArgX = 0f;
+            double secondArgZ = 0f;
+
+            double result1 = 0.000f;
+            double result2 = 0.000f;
+            double result3 = 0.000f;
+
+            while (isResultable != true)
+            {
+                try
+                {
+                    Console.WriteLine("Ввод данных для первой задачи (Х):");
+                    string[] varArray = Console.ReadLine().Split();
+
+                    if (varArray.Length == 1)
+                    { 
+                        firstArgX = double.Parse(varArray[0]);
+                        Array.Clear(varArray, 0, varArray.Length);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Больше(меньше) 1-го аргумента!");
+                        Console.ReadKey();
+                        throw new System.ArgumentException("smthn");
+                    }
+
+                    Console.WriteLine("Ввод данных для второй задачи (дни, скидка в процентах, сумма):");
+                    varArray = Console.ReadLine().Split();
+
+                    if (varArray.Length == 3)
+                    {
+                        days = int.Parse(varArray[0]);
+                        discount = double.Parse(varArray[1]);
+                        sum = double.Parse(varArray[2]);
+                        Array.Clear(varArray, 0, varArray.Length);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Больше(меньше) 3-х аргументoв!");
+                        Console.ReadKey();
+                        throw new System.ArgumentException("smthn");
+                    }
+
+                    Console.WriteLine("Ввод данных для третей задачи (Х, Z):");
+                    varArray = Console.ReadLine().Split();
+                    if (varArray.Length == 2)
+                    {
+                        secondArgX = double.Parse(varArray[0]);
+                        secondArgZ = double.Parse(varArray[1]);
+                        Array.Clear(varArray, 0, varArray.Length);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Больше(меньше) 1-го аргумента!");
+                        Console.ReadKey();
+                        throw new System.ArgumentException("smthn");
+                    }
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("Что-то пошло не так... Повторите ввод данных");
+                    continue;
+                }
+
+                try
+                {
+                    result1 = Math.Sqrt(firstArgX + 2) - Math.Sqrt(firstArgX + 3);
+                    
+                    for (int i = 1; i < days; i++)
+                    {
+                        sum = (sum += 3) * (100 + discount);
+                    }
+                    result2 = sum;
+
+                    if (Math.Pow(secondArgX, 2) * Math.Abs(secondArgX - secondArgZ) == 1) throw new Exception();
+
+                    result3 = (secondArgX + Math.Exp(secondArgZ - 1)) / 
+                        (1 - Math.Pow(secondArgX, 2) * Math.Abs(secondArgX - secondArgZ));
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("Расчеты выполнить невозможно, повторите попытку.");
+                    continue;
+                }
+
+                Console.Clear();
+                Console.WriteLine($"Результат 1й задачи: {result1}\nРезультат 2й задачи: {result2}\nРезультат 3й задачи: {result3}\n Область вызначения функции: X² ∙ |X - Z| ≠ 1");
+               
+                isResultable = true;
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Programming Basics/lab_4/lab.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace lab_4
+{
+    public class lab
+    {
+
+        static void Main(string[] args)
+        {
+            float a =2f, b = 4f;
+            lab Program = new lab();
+
+            Console.WriteLine("Площа фігури за малюнком даного завдання");
+            Console.WriteLine(Program.RectangSqr(a, b));
+            Console.ReadKey();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Введіть свої значення (сторони)");
+                string A = Console.ReadLine(), B = Console.ReadLine();
+
+                try
+                {
+                    Console.WriteLine(Program.RectangSqr( float.Parse(A), float.Parse(B) ));
+                    break;
+                }
+                catch
+                {
+                    continue;
+                }
+            };
+            Console.ReadKey();
+
+            //----------------------------------------------------//
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Введіть свої значення для коеффіцієнти кв. рівняння(A>0, B, C)");
+
+                string A = Console.ReadLine(), B = Console.ReadLine(), C = Console.ReadLine();
+                try
+                {
+                    bool Task2 = Program.SqrEquation( float.Parse(A), float.Parse(B), float.Parse(C) );
+
+                    if (Task2 == true) 
+                        break;
+                } 
+                catch
+                {
+                    continue;
+                }
+                
+            }
+
+        }
+        float RectangSqr(float sideA, float sideB)
+        {
+            return sideA * sideB;
+        }
+
+        bool SqrEquation(double fA, double fB, double fC)
+        {
+            if (fA <= 0) 
+                return false;
+
+            double D = fB * fB - 4 * fA * fC, X1, X2;
+
+            if (D <= 0) 
+                return false;
+            else
+            {
+                X1 = (-fB - Math.Sqrt(D)) / (2 * fA);
+
+                X2 = (-fB + Math.Sqrt(D)) / (2 * fA);
+                if (X1 > X2) 
+                    Console.WriteLine(X1 + " " + X2);
+                else
+                    Console.WriteLine(X2 + " " + X1);
+            }
+            return true;
+        }
+
+
+    }
+}
